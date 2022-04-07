03fd9a8a9fda2410bda8658ea8cc0cb2d7b29579
rsteube <rsteube@users.noreply.github.com>
2022-04-07 21:58:34 +0200

    tmp

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "MesloLGS Nerd Font";
+static char *font = "MesloLGS Nerd Font Mono";
 static int borderpx = 2;
 
 /*
