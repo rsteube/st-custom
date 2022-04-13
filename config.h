146bec6624d64351fa20bee7f99a901dc4261829
rsteube <rsteube@users.noreply.github.com>
2022-04-13 18:50:46 +0200

    fix externalpipe

--- config.h
+++ config.h
@@ -188,6 +188,10 @@ static MouseShortcut mshortcuts[] = {
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
+static char *openurlcmd[] = { "/bin/sh", "-c",
+    "xurls | sort | uniq | dmenu -l 10 | xargs -r xdg-open",
+    "externalpipe", NULL };
+
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
 	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
@@ -203,6 +207,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+        { MODKEY,               'u',            externalpipe,   { .v = openurlcmd } },
 };
 
 /*
